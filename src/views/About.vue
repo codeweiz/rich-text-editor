f26d483e9ae709fb92701619bd380c988166b364
codeweiz <13955645241@163.com>
2021-07-14 17:25:59 +0800

    about页面不可用

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,24 +1,10 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <div id="editor" class="editor"></div>
   </div>
 </template>
-
 <script>
-import Editor from 'wangeditor'
 export default {
-  data() {
-    return{
-      editor: ''
-    }
-  },
-  mounted() {
-    this.editor = new Editor('#editor');
-    this.editor.custom.uploadImgShowBase64 = true;
-    this.editor.custom.showLinkImg = false;
-    this.editor.create();
-    this.editor.txt.html();
-  }
+  name: "about"
 }
 </script>
